d730685a12ba2f61e2606dbcab1095b009048d8a
Collinsoden22 <Collinsoden22@gmail.com>
2022-11-12 07:44:12 -0800

    Task 1 Corrected

--- web_flask/1-hbtn_route.py
+++ web_flask/1-hbtn_route.py
@@ -13,6 +13,11 @@ def hello_hbnb():
     displays text Returns:  text
     """
     return "Hello HBNB!"
+@app/route("/hbnb", strict_slashes=False)
+
+def display_text():
+    #Dislay the next line for this route
+    return "HBTN"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
